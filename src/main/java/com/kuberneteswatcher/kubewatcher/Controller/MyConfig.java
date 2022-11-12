184fa8a228580a6f88520e2b3812c431229fdc82
surendharramaraj <surendharramaraj@gmail.com>
2022-11-13 00:16:37 +0530

    updated jar

--- src/main/java/com/kuberneteswatcher/kubewatcher/Controller/MyConfig.java
+++ src/main/java/com/kuberneteswatcher/kubewatcher/Controller/MyConfig.java
@@ -1,7 +1,6 @@
 package com.kuberneteswatcher.kubewatcher.Controller;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
